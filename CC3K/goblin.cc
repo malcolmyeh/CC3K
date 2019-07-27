0589276129a64ae32b62ae14d19f28771c242579
malcolmyeh <malcolmyeh@gmail.com>
2019-07-26 20:56:35 -0400

    fix enemy constructor

--- CC3K/goblin.cc
+++ CC3K/goblin.cc
@@ -1,5 +1,5 @@
 #include "goblin.h"
 
-Goblin::Goblin(Posn position, bool hasCompass, int chamberID) : Atk(5), Def(10), HP(70), gold(1), race("Goblin"), symbol('N'), hasCompass(hasCompass), chamberID(chamberID) {}
+Goblin::Goblin(Posn position, bool hasCompass, int chamberID) : Atk(5), Def(10), HP(70), gold(1), race("Goblin"), symbol('N'), hasCompass(false), chamberID(chamberID), position(position) {}
 
 Goblin::~Goblin(){}
